a78d7cdb2f6e9fb1daf7de410eb7df126bdc11c5
AI-prodev <genai.fullstack@gmail.com>
2019-08-26 22:33:47 -0500

    add working endpoint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 const CHASE_CASHBACK = require("./cards/chase-cashback");
 
-class Cards {
-    getChaseCashBackCal(callback) {
-        let cal = new CHASE_CASHBACK();
-        return cal.getCalendar(callback);
-    }
-}
-
-module.exports = Cards;
+module.exports.getChaseCashBackCal = (callback) => {
+    let cal = new CHASE_CASHBACK();
+    return cal.getCalendar(callback);
+};
