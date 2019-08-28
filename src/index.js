20579a9a14f70014d5eadd6839c67fecbc44d496
AI-prodev <genai.fullstack@gmail.com>
2019-08-27 22:22:05 -0500

    add readmes and repo link to npm

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ const CHASE_CASHBACK = require("./cards/chase-cashback");
 const DISCOVER_CASHBACK = require("./cards/discover-cashback");
 
 
-module.exports.getChaseCashBackCal = (callback) => {
-    let cal = new CHASE_CASHBACK();
-    return cal.getCalendar(callback);
-};
+// module.exports.getChaseCashBackCal = (callback) => {
+//     let cal = new CHASE_CASHBACK();
+//     return cal.getCalendar(callback);
+// };
+//
+// module.exports.getDiscoverCashBackCal = (callback) => {
+//     let cal = new DISCOVER_CASHBACK();
+//     return cal.getCalendar(callback);
+// };
 
-module.exports.getDiscoverCashBackCal = (callback) => {
-    let cal = new DISCOVER_CASHBACK();
-    return cal.getCalendar(callback);
-};
+let cal = new CHASE_CASHBACK();
+cal.getCalendar((err, result) => {
+    console.log(result);
+});
